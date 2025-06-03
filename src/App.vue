78675e3e088749b96efd63c59a9e91b77e7146a7
Adrian <yy_1123@hotmail.com>
2025-06-03 17:40:59 +0800

    functions and computed values

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,15 @@
 
     <!-- <p>{{ jobs[0].location }}</p> -->
 
-    <JobList :jobs="jobs" />
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header>
+
+    <JobList :jobs="jobs" :order="order" />
   </div>
 </template>
 
@@ -15,6 +23,7 @@
 import { defineComponent, reactive, ref, toRefs } from "vue";
 import JobList from "./components/JobList.vue";
 import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
 
 export default defineComponent({
   name: "App",
@@ -50,7 +59,13 @@ export default defineComponent({
       { title: "Body Guard", location: "Kuala Lumpur", salary: 50000, id: "5" },
     ]);
 
-    return { jobs };
+    const order = ref<OrderTerm>("title");
+
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+
+    return { jobs, handleClick, order };
   },
   // data() {
   //   return {
@@ -72,4 +87,20 @@ export default defineComponent({
 </script>
 
 <style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
 </style>
